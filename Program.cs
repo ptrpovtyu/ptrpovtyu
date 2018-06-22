5ba721cfc059a900b8fd870b44381cbbe6de5970
ptrpov <hgrpov@amp.ptrpov.com>
2018-06-23 01:18:24 +0700

    Initial commit

--- Program.cs
+++ Program.cs
@@ -46,6 +46,16 @@ namespace ptrpov
             WorkingWithIntegers();
 
             OrderPrecedence();
+            int a = 7;
+            int b = 4;
+            int c = 3;
+            int d = (a  + b) / c;
+            int e = (a + b) % c;
+            Console.WriteLine($"quotient: {d}");
+            Console.WriteLine($"remainder: {e}");
+            int max = int.MaxValue;
+            int min = int.MinValue;
+            Console.WriteLine($"The range of integers is {min} to {max}");
 
         }
     }
