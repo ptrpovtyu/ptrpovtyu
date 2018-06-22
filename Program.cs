2638b53a61f926e5cf59b4aca6368955655de813
ptrpov <hgrpov@amp.ptrpov.com>
2018-06-23 01:12:36 +0700

    Initial commit

--- Program.cs
+++ Program.cs
@@ -20,23 +20,33 @@ namespace ptrpov
             Console.WriteLine(c);
         }
 
-        static void Main(string[] args)
-        {
-           // WorkingWithIntegers();
+        static void OrderPrecedence()
+        {   
             int a = 5;
             int b = 4;
             int c = 2;
             int d = a + b * c;
             Console.WriteLine(d);
+
             d = (a  + b) * c;
             Console.WriteLine(d);
+
             d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
             Console.WriteLine(d);
+
             int e = 7;
             int f = 4;
             int g = 3;
             int h = (e  + f) / g;
             Console.WriteLine(h);
+        }
+
+        static void Main(string[] args)
+        {
+            WorkingWithIntegers();
+
+            OrderPrecedence();
+
         }
     }
 }
