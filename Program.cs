dc94ddbbb08ec6e06421112a853ac05f6c111282
ptrpov <hgrpov@amp.ptrpov.com>
2018-06-22 16:20:16 +0700

    Initial commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace ptrpov
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Press any key to continue...");
+            Console.ReadKey(true);
         }
     }
 }
