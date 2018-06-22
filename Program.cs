2c086b3d71a9c3860226a900ffd28d047076e9dc
ptrpov <hgrpov@amp.ptrpov.com>
2018-06-23 01:06:20 +0700

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,13 +1,35 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ptrpov
 {
     class Program
     {
+        static void WorkingWithIntegers()
+        {
+            int a = 18;
+            int b = 6;
+            int c = a + b;
+            Console.WriteLine(c);
+            c = a - b;
+            Console.WriteLine(c);
+            c = a * b;
+            Console.WriteLine(c);
+            c = a / b;
+            Console.WriteLine(c);
+        }
+
         static void Main(string[] args)
         {
-            Console.Write("Press any key to continue...");
-            Console.ReadKey(true);
+           // WorkingWithIntegers();
+            int a = 5;
+            int b = 4;
+            int c = 2;
+            int d = a + b * c;
+            Console.WriteLine(d);
+            d = (a  + b) * c;
+            Console.WriteLine(d);
         }
     }
 }
