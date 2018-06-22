dcd0c66b4e0bf42a2ad3952fdfdb38c53f93b3f9
ptrpov <hgrpov@amp.ptrpov.com>
2018-06-23 01:20:31 +0700

    Initial commit

--- Program.cs
+++ Program.cs
@@ -56,6 +56,8 @@ namespace ptrpov
             int max = int.MaxValue;
             int min = int.MinValue;
             Console.WriteLine($"The range of integers is {min} to {max}");
+            int what = max + 3;
+            Console.WriteLine($"An example of overflow: {what}");
 
         }
     }
