277420959080edf153880b5c9430f3fd69f66eda
ptrpov <hgrpov@amp.ptrpov.com>
2018-06-23 11:24:16 +0700

    Initial commit

--- Program.cs
+++ Program.cs
@@ -8,7 +8,12 @@ namespace ptrpov
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 5;
+            int b = 3;
+            if (a + b > 10)
+            Console.WriteLine("The answer is greater than 10");
+            else
+            Console.WriteLine("The answer is not greater than 10");
         }
     }
 }
