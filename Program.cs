14a1ab63025fb4ec6afa29dcb7cb6aa43e97ba2b
ptrpov <hgrpov@amp.ptrpov.com>
2018-06-23 11:30:42 +0700

    Initial commit

--- Program.cs
+++ Program.cs
@@ -11,9 +11,24 @@ namespace ptrpov
             int a = 5;
             int b = 3;
             if (a + b > 10)
-            Console.WriteLine("The answer is greater than 10");
+            {
+                Console.WriteLine("The answer is greater than 10");
+            }
             else
+            {
             Console.WriteLine("The answer is not greater than 10");
+            }
+            int c = 4;
+            if ((a + b + c > 10) && (a > b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("And the first number is greater than the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("Or the first number is not greater than the second");
+            }
         }
     }
 }
